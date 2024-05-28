c7c97990d51f3a18396048c400d150c17b8eecf1
iyi <jwj@wenjunjiang.com>
2024-05-28 12:19:00 +0800

    test:isolate

---
+++ lib/pages/dialog/dialog.dart
@@ -0,0 +1,9 @@
+// import 'package:flutter/material.dart';
+//
+// Future<bool?> _showAsyncDialog() {
+//   return showDialog<bool>(
+//       context: contenxt,
+//       builder: (context) {
+//         return const AlertDialog();
+//       });
+// }
---
+++ test/isolates_test.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:isolate';
+
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('test isolate', (WidgetTester widgetTester) async {
+    print(Isolate.current.debugName);
+  });
+  testWidgets('new isolate', (WidgetTester widgetTester) async {
+    Isolate timeIsolate;
+    final mainControlPort = ReceivePort()
+      ..listen((message) {
+        print('send back to mainIsolate: $message');
+      });
+    void timerTick(SendPort sendPort) async {
+      print('Current isolate ${Isolate.current.debugName} start');
+      Timer.periodic(const Duration(seconds: 1), (timer) {
+        final ts = DateTime.now().toIso8601String();
+        sendPort.send(ts);
+      });
+    }
+
+    timeIsolate = await Isolate.spawn(timerTick, mainControlPort.sendPort);
+  });
+}
